c7cfae0d27bea7d1f3d9419e59f4dba2edd846ea
sachinsaxena1996 <sachinsaxena1996@gmail.com>
2019-03-18 20:01:43 +0530

    Updates

---
+++ precache-manifest.3302175b97bf05c9d5b20b22d74ff669.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9198ae3b868f769fc8ea",
+    "url": "/pics/static/js/runtime~main.7218180c.js"
+  },
+  {
+    "revision": "57277c61e2fe14ada60c",
+    "url": "/pics/static/js/main.f0840369.chunk.js"
+  },
+  {
+    "revision": "e053a1da17cbfeebd8e3",
+    "url": "/pics/static/js/2.9176d4f9.chunk.js"
+  },
+  {
+    "revision": "57277c61e2fe14ada60c",
+    "url": "/pics/static/css/main.d10b74cb.chunk.css"
+  },
+  {
+    "revision": "0f055fadf5e7cf14edb517a18fa3ef4e",
+    "url": "/pics/index.html"
+  }
+];
